b517298c9eb1b21e3d207ef69400b9f10440d129
Nickolas Nikolic <nickolasnikolic@gmail.com>
2015-10-23 14:53:37 -0500

    pruning

--- public/app/js/controllers.js
+++ public/app/js/controllers.js
@@ -1,6 +1,6 @@
 blindApp.controller('IndexController', ['$scope', '$state', 'globals', function($scope, $state, globals) {}])
 
-blindApp.controller('HomeController', ['$scope', '$state', '$http', '$sce', 'globals', function($scope, $state, $http, $sce, globals) {
+blindApp.controller('HomeController', ['$scope', '$state', '$http', 'globals', function($scope, $state, $http, globals) {
 
   $scope.getItems = function( keywords ){
       $http.get('/api/az/' + keywords)
